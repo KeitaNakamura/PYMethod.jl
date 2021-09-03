85f3a10b264895282ba7c6c3e8e30843b1a9789b
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-09-03 22:35:14 +0900

    Add FEM tests for unlinear springs (#10)

--- test/fem.jl
+++ test/fem.jl
@@ -7,7 +7,8 @@ function deflection_chang(H::Real, D::Real, E::Real, I::Real, k_h::Real, h::Real
     end
 end
 
-@testset "Compare with Chang's equation" begin
+@testset "FEM: Linear spring" begin
+    # compare with Chang's equation
     pile = FEPileModel(0, 20, 400)
     D = 0.6
     E = 2e8
@@ -23,5 +24,38 @@ end
     deflection(z) = deflection_chang(H, D, E, I, k_h, 1, 19-z)
     for i in 1:length(pile.u)
         @test pile.u[i] ≈ deflection(pile.coordinates[i])  atol = 1e-4
+    end
+end
+
+@testset "FEM: Unlinear spring" begin
+    @testset "not depending on depth" begin
+        pile = FEPileModel(0, 20, 500)
+        pile.E .= 1.0e7
+        pile.I .= 1.0e-3
+        pile.D .= 0.5
+        pile.Fext[1] = 1000.0
+        k = 2000
+        pile.pycurves .= pycurve(y, z) = z > 19 ? 0 : k*y^0.5
+        solve!(pile)
+        @test log10(pile.u[1]) ≈ 0.1536   atol = 1e-2
+        @test log10(pile.u[26]) ≈ -0.0529 atol = 1e-2
+        @test log10(abs(pile.θ[1])) ≈ -0.2554  atol = 1e-2
+        @test log10(abs(pile.θ[26])) ≈ -0.2964 atol = 1e-2
+        @test log10(maximum(abs, pile.M)) ≈ 3.2055 atol = 1e-2
+    end
+    @testset "depending on depth" begin
+        pile = FEPileModel(0, 20, 100)
+        pile.E .= 1.0e7
+        pile.I .= 1.0e-3
+        pile.D .= 0.5
+        pile.Fext[1] = 1000
+        k = 2000
+        pile.pycurves .= pycurve(y, z) = z > 19 ? 0 : k*(19-z)*y^0.5
+        solve!(pile)
+        @test log10(pile.u[1]) ≈ 0.2391  atol = 1e-3
+        @test log10(pile.u[6]) ≈ 0.0385  atol = 1e-3
+        @test log10(abs(pile.θ[1])) ≈ -0.1817 atol = 1e-3
+        @test log10(abs(pile.θ[6])) ≈ -0.2161 atol = 1e-3
+        @test log10(maximum(abs, pile.M)) ≈ 3.3128  atol = 1e-3
     end
 end
